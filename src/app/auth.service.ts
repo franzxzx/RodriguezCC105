eb6ceaa6ba3be60a30c0d558b53239722cf48364
franzxzx <franz092121@gmail.com>
2023-12-09 12:02:55 +0800

    fixing bugs

---
+++ src/app/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from '@firebase/auth';
+import { Observable, from, switchMap } from 'rxjs';
+import { authState } from '@firebase/auth'; // or from the correct file
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  currentUser$ = authState(this.auth); 
+  user$: Observable<any>;
+
+  constructor(private auth: Auth) {
+    this.user$ = new Observable();
+  }
+
+  signUp(username:string, email: string, password: string) {
+    return from(createUserWithEmailAndPassword(this.auth, email, password)).pipe( 
+      switchMap(({user}) => updateProfile(user, {displayName: username})));
+  }
+
+login(email: string, password: string): Observable<any> {
+  return from(signInWithEmailAndPassword(this.auth, email, password));
+}
+
+logout(): Observable<any>{
+return from(this.auth.signOut());
+}
+}
